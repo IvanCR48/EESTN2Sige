dd0c293f5313bdee9a1a1448885b8f419f37acb5
Iván Ismael Cardozo <cardozoivanismael@gmail.com>
2026-06-28 14:19:23 -0300

    Add files via upload

---
+++ logs/error.log.php
@@ -0,0 +1,2 @@
+<?php exit; ?>
+{"timestamp":"2026-06-07 17:09:25","tipo":"ERROR","mensaje":"Esto es una prueba de log de error","archivo":"C:UsersAdmin.geminiantigravitybrain10f8c56a-4995-4968-9374-1b257b475bacscratchlog_some_events.php","linea":13,"ip":"127.0.0.1","usuario_id":null,"contexto":{"test_context":"ok"}}
